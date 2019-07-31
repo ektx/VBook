9db4140553790ccb6be972d605cc499d69937477
wuming <530675800@qq.com>
2019-07-31 11:43:03 +0800

    :art: 布局调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,36 +1,60 @@
 <template>
     <div class="home-view">
         <header>
-
+            <div class="logo-box">
+                <img src="/logo.svg" alt="VBook">
+                <h1>VBook</h1>
+            </div>
+            <div class="search-box">
+                <input type="text">
+            </div>
         </header>
-        <main>
-            <MarkdownIt :value="inner"/>
-        </main>
+        <section class="content">
+            <aside>
+                <Navs :value="navs" @change="changeEvt"/>
+            </aside>
+            <main>
+                <MarkdownIt :value="inner"/>
+            </main>
+        </section>
     </div>
 </template>
 
 <script>
 import MarkdownIt from '@/components/markdownIt'
+import Navs from '@/components/navs'
+import navs from '@/navs.js'
 
 export default {
     name: "home",
     components: {
-        MarkdownIt
+        MarkdownIt,
+        Navs
     },
     data() {
         return {
-            inner: '# hello'
+            inner: '# VBook\n请选择菜单内容',
+            navs
         }
     },
     mounted () {
-        this.$axios({
-            url: '/doc/layer.md',
-            method: 'GET'
-        }).then(res => {
-            this.inner = res.data
-        })
+        
     },
     methods: {
+        changeEvt (nav) {
+            console.log(nav.label)
+            if ('file' in nav)
+                this.getFile(nav.file)
+        },
+
+        getFile (file) {
+            this.$axios({
+                url: `/doc/${file}.md`,
+                method: 'GET'
+            }).then(res => {
+                this.inner = res.data
+            })
+        }
     }
 };
 </script>
@@ -42,7 +66,10 @@ export default {
         top: 0;
         left: 0;
         right: 0;
+        display: flex;
+        align-items: center;
         height: 60px;
+        padding: 0 30px;
         background: #fff;
         border-bottom: 1px solid #ddd;
 
@@ -52,8 +79,40 @@ export default {
         }
     }
 
-    & > main {
-        margin: 60px 0 0;
+    .logo-box {
+        flex: 1;
+
+        img {
+            display: inline-block;
+            width: 24px;
+            margin: 0 10px 0 0;
+            vertical-align: top;
+        }
+        h1 {
+            display: inline-block;
+            color: #273849;
+        }
+    }
+
+    .content {
+        display: flex;
+        flex-direction: row;
+
+        & > aside {
+            padding: 80px 0 20px;
+            height: 100vh;
+            width: 300px;
+            overflow: auto;
+            box-sizing: border-box;
+        }
+
+        & > main {
+            flex: 1;
+            height: 100vh;
+            padding: 60px 20px 20px;
+            overflow: auto;
+            box-sizing: border-box;
+        }
     }
 }
 </style>
