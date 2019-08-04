a1a0f4266c43ae00c7f4dd73f14d3b534b7a69aa
wuming <530675800@qq.com>
2019-08-04 17:37:38 +0800

    删除引用组件

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ import 'highlight.js/styles/vs.css'
 
 // 你在本示例中要使用的组件库
 // 这是我们添加一个本地开发中的库
-import VC from '@ektx/vc'
-import '@ektx/vc/dist/vc.css'
 
 Vue.prototype.$axios = axios
 Vue.prototype.$compile = Vue.compile
